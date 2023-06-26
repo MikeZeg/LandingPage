b35bd911b96842440910a88095213fa06794dc3f
MikeZeg <michalzegzula@gmail.com>
2023-06-26 20:17:59 +0100

    add aminiations to scss

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,14 @@
 // alert('hello world!')
 
 const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header');
 
 btnHamburger.addEventListener('click', function(){
     console.log('open menu');
 
-    if(btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open');
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
     }else{
-        btnHamburger.classList.add('open');
+        header.classList.add('open');
     }
 })
