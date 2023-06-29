32b1b6a877f00f68ea8215f5d560f6e17c33feff
MikeZeg <michalzegzula@gmail.com>
2023-06-29 19:56:37 +0100

    edit HTML + styles + JS animation

--- app/js/script.js
+++ app/js/script.js
@@ -2,20 +2,28 @@
 
 const btnHamburger = document.querySelector('#btnHamburger')
 const header = document.querySelector('.header');
+const body = document.querySelector('body')
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll(".has-fade")
 
 btnHamburger.addEventListener('click', function(){
     console.log('open menu');
 
-    if(header.classList.contains('open')){ // Close Hamburger Menu
+    if(header.classList.contains('open')) { // Close Hamburger Menu
+        body.classList.remove('nonscroll')
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out')
-        
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out')
+
+        })
 
     }else{ // Open Hamburger Menu
+        body.classList.add('nonscroll')
         header.classList.add('open');
-        overlay.classList.remove('fade-out')
-        overlay.classList.add('fade-in')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        });
     }
 })
